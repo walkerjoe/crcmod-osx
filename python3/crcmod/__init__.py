e44e4a4d30bce61d14a37d7bf70625752c0a3b10
Joe Walker <walkerjoseph@google.com>
2019-03-07 08:37:06 -0800

    Initial commit for current branch -- adding files

---
+++ python3/crcmod/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from crcmod.crcmod import _usingExtension
+    from crcmod.crcmod import *
+    import crcmod.predefined
+except ImportError:
+    # Make this backward compatible
+    from crcmod import *
+    import predefined
+__doc__ = crcmod.__doc__
